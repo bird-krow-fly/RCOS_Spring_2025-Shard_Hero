404842e24a9d5459d6b3a6d4b72089a00d0d96b6
Arjun Kandadai <arjun.kandadai@gmail.com>
2025-03-11 17:22:22 -0400

    Scrapped it all. Simple camera that directly follows character. Fixed the issue of floor disappearing with jumps

--- Camera Work/RCOS-S25-Learning-Godot-main/Godot Practice/2d-platformer-game/game_manager.gd
+++ Camera Work/RCOS-S25-Learning-Godot-main/Godot Practice/2d-platformer-game/game_manager.gd
@@ -2,30 +2,33 @@ extends Node
 
 
 @onready var points_label: Label = %PointsLabel
-#@onready var pathFollow : PathFollow2D = $Path2D/PathFollow2D
 @onready var player = get_node("../SceneObjects/CharacterBody2D")
 @onready var camera = get_node("../SceneObjects/MovingCamera")
 @onready var y_pos = player.position.y;
 
 func _process(delta):
-	var direction: Vector2 = Input.get_vector("left","right","jump","down");
+	#print("Player y Vel: ",player.velocity.y)
+	print("Player y Pos: ", player.position.y)
+	print("Camera y Pos: ", camera.position.y)
+	if player.position.y < 576 and player.position.y > 575:
+		camera.position.y = 405
+	#var direction: Vector2 = Input.get_vector("left","right","jump","down");
 	if player.is_on_floor():
 		y_pos = player.position.y;
 		#camera.position.y = -400;
 		
 	if not player.is_on_floor():
-		if player.position.y > y_pos+100 or player.position.y < y_pos-100:
-			#camera.position.y = -player.get_position()[1];
-			camera.position.y += (player.velocity.y)/130;
-			if camera.position.y > -405: 
-				camera.position.y = -405
+		if player.position.y > y_pos or player.position.y < y_pos:
+			camera.position.y += player.velocity.y/100;
+			if camera.position.y >= 405: 
+				camera.position.y = 405
 	
 	if Input.is_action_pressed("right"):
 		#print("Character X Position: ",player.position.x);
 		#print("Camera X Position:    ",camera.position.x);
 		#print("Character Y Position: ",player.position.y);
-		print("Camera Y Position:    ",camera.position.y);
-		if player.position.x < 1745 && player.position.x > 0:
+		#print("Camera Y Position:    ",camera.position.y);
+		if player.position.x < 2147 && player.position.x > 520:
 			if player.velocity[0] != 0:
 			#var direction: Vector2 = Input.get_vector("left","right","up","down");
 				camera.position.x = player.get_position()[0] - 80;
@@ -34,9 +37,9 @@ func _process(delta):
 		#print("Character X Position: ",player.position.x);
 		#print("Camera X Position:    ",camera.position.x);
 		#print("Character Y Position: ",player.position.y);
-		print("Camera Y Position:    ",camera.position.y);
+		#print("Camera Y Position:    ",camera.position.y);
 		if player.velocity[0] != 0:
-			if player.position.x > 0 && player.position.x < 1745:
+			if player.position.x > 520 && player.position.x < 2147:
 				camera.position.x = player.get_position()[0] - 80;
 
 var points = 0
